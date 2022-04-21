46bc8376e8537cb0dd58293cda5e0bc1a5fd5d7a
Matias Azcui <matias.azcui@gmail.com>
2022-04-21 12:15:00 -0300

    Foto gaston

--- src/__mocks__/curriculums.jsx
+++ src/__mocks__/curriculums.jsx
@@ -4,6 +4,7 @@ const curriculumsMock = [
     puesto: "Presidente",
     bio: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
     link: "https://www.linkedin.com/in/gaston-adhemar-1480554",
+    imagen: "./images/gaston.jpeg",
   },
   {
     nombre: "Federico Mayol",
